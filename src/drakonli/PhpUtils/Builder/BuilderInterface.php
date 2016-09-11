77a7fe07403ea4e4b62623e7edcd0bc300940c2e
drakonli <drakonli@mail.ru>
2016-09-12 02:43:06 +0300

    + removed reset method from builder + builder interface now is just a tagging service

--- src/drakonli/PhpUtils/Builder/BuilderInterface.php
+++ src/drakonli/PhpUtils/Builder/BuilderInterface.php
@@ -3,15 +3,11 @@
 namespace drakonli\PhpUtils\Builder;
 
 /**
+ * Tagging service
+ *
  * @author    drakonli - Arthur Vinogradov - <artur.drakonli@gmail.com>
  * @link      www.linkedin.com/in/drakonli
  */
 interface BuilderInterface
 {
-    /**
-     * Resets built object to start building again from scratch
-     *
-     * @return static
-     */
-    public function reset();
 }
